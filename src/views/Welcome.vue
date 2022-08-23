2292ecfaaa58d358ba27030368e57dc9c416ee24
hirokazu <n.hirokazu92@gmail.com>
2022-08-23 21:45:18 +0900

    Vue.js_基本操作

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,17 +1,28 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p v-if="isEnabled">{{ subtitle }}</p>
-    <button @dblclick="toggle">トグルする</button>
-    <p>{{ text }}</p>
+    <!-- データを子コンポーネントに渡している -->
+    <!-- :<props名>="<データの名前>" -->
+    <!-- <TestComponent :isEnabled="isEnabled" message="コンポーネントにデータが渡されています" /> -->
+
+    <!-- toggleという名前に一致する親コンポーネントのメソッドが実行される -->
+    <!-- @toggleは子コンポーネントのthis.$emit('toggle')に紐づいている -->
+    <!-- "toggle"はWelcome.vueのメソッド名であるtoggleに紐づいている -->
+    <TestComponent @toggle="toggle" message="コンポーネントにデータが渡されています" />
+    <p v-if="isEnabled">こんにちは</p>
   </div>
 </template>
 
 <script>
+import TestComponent from '../components/TestComponent.vue'
+
 export default {
+  components: {
+    TestComponent
+  },
   data() {
     return {
-      title: '初めてのVue.jsアプリです！',
+      title: '初めてのVue.jsアプリです!',
       subtitle: 'ようこそ',
       isEnabled: true
     }
@@ -32,3 +43,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  p {
+    font-size: 20px;
+    color: red;
+  }
+</style>
