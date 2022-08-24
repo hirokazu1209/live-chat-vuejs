fcd2a6b1726eb86a8cce1bc5283b63e618ab0b89
hirokazu <n.hirokazu92@gmail.com>
2022-08-24 23:49:57 +0900

    アクセスできるページの制御

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome'
 import Chatroom from '../views/Chatroom'
+import useValidate from '../auth/validate'
+
+const { error, validate } = useValidate()
+const requireAuth = async(to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('accessToken')
+
+  if(!uid || !client || !accessToken) {
+    console.log('ログインしていません')
+    next({ name: 'Welcome' })
+    return
+  }
+
+  await validate()
+
+  // beforeAuthで呼ばれるメソッドは、nextメソッドの実行が必ず必要のため記述
+  if(error.value) {
+    console.log('認証に失敗しました')
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+}
+
+const noRequireAuth = async(to, from, next) => {
+  const uid = window.localStorage.getItem('uid')
+  const client = window.localStorage.getItem('client')
+  const accessToken = window.localStorage.getItem('accessToken')
+
+  if(!uid && !client && !accessToken ) {
+    next()
+    return
+  }
+
+  await validate()
+
+  if(!error.value) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Welcome',
-    component: Welcome
+    component: Welcome,
+    beforeEnter: noRequireAuth
   },
   {
     path: '/chatroom',
     name: 'Chatroom',
-    component: Chatroom
+    component: Chatroom,
+    // ルートにアクセスする前に実行されるメソッド
+    beforeEnter: requireAuth
   }
 ]
 
